7c87ae82b6bd32f6946a5681f0f6e4a002df3b29
tthtmi <75137805+tthtmi@users.noreply.github.com>
2021-10-13 06:23:16 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -7,29 +7,3 @@ myImage.onclick = function() {
       myImage.setAttribute('src', 'images/nerv.jpg');
     }
 }
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-function setUserName() {
-    let myName = prompt('请输入你的名字。');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = '欢迎回来，' + myName;
-  }
-  if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = '欢迎回来，' + storedName;
-  }
-  myButton.onclick = function() {
-    setUserName();
- }
- function setUserName() {
-    let myName = prompt('请输入你的名字。');
-    if(!myName || myName === null) {
-      setUserName();
-    } else {
-      localStorage.setItem('name', myName);
-      myHeading.innerHTML = '欢迎回来，' + myName;
-    }
-  }
